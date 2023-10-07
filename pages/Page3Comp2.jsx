c1e8e2edb351ddafced643dfed82973ac11cbfe2
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2023-10-07 16:28:42 +0530

    .

--- pages/Page3Comp2.jsx
+++ pages/Page3Comp2.jsx
@@ -1,8 +1,8 @@
 const Page3Comp2 = (props) => {
   return (
     <div>
-        <div className="w-[275px] h-[266px] flex flex-col justify-center bg-[#151721]">
-            <p className="text-6xl font-mono text-white text-center ">{props.title}</p>
+        <div className="w-[265px] h-[266px] flex flex-col justify-center bg-[#151721]">
+            <p className="text-5xl font-sans font-bold text-white text-center ">{props.title}</p>
             <p className="text-slate-200 text-xl text-center mt-4">{props.text}</p>
         </div>
     </div>
