1493b5794c66ceadfaa670a88f608f9554ffd3da
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2023-10-08 18:51:33 +0530

    .

---
+++ pages/Events.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+const Events = () => {
+  return (
+    <div className="">
+        <h1 className="text-8xl text-center">EVENTS PAGE</h1>
+    </div>
+  )
+}
+
+export default Events
--- pages/Navbar.jsx
+++ pages/Navbar.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 const Page1 = () => {
   return (
     <div>
@@ -15,7 +16,7 @@ const Page1 = () => {
                 />
               </div>
               <div className="text-white font-mono text-xl flex gap-6 ms-auto me-40 ">
-                <p className="hover:text-[#7686DB] hover:cursor-pointer hover:underline-offset-auto">Home</p>
+                <p className="hover:text-[#7686DB] hover:cursor-pointer hover:underline-offset-auto"><Link href="/Events">Home</Link></p>
                 <p className="hover:text-[#7686DB] hover:cursor-pointer hover:underline-offset-auto">About Us</p>
                 <p className="hover:text-[#7686DB] hover:cursor-pointer hover:underline-offset-auto">Services</p>
                 <p className="hover:text-[#7686DB] hover:cursor-pointer hover:underline-offset-auto">Products</p>
