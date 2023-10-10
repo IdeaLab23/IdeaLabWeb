a9386b3eeb0615b62b7344a03f6a74d604f52d18
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2023-10-10 07:24:13 +0530

    complete code

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 import Page1 from "@/pages/Page1"
 import Page2 from "@/pages/Page2"
 import Page3 from "@/pages/Page3"
+import Page4 from "@/pages/page4"
+import Page5 from "@/pages/page5"
+import Page6 from "@/pages/page6"
 
 export default function Home() {
   return (
@@ -8,6 +11,9 @@ export default function Home() {
       <Page1 />
       <Page2 />
       <Page3 />
+      <Page4 />
+      <Page5 />
+      <Page6 />
     </>
   )
 }
---
+++ pages/page4.jsx
@@ -0,0 +1,38 @@
+import Services_comp from "./services_comp"
+import Image from "next/image"
+const Page4 = (props) => {
+const data=
+  [
+    "Augue congue turpis ut purus ut nibh sit. Et consectetur elit volutpat blandit sed facilisis. At in orci vel a vestibulum egestas. Ut faucibus.",
+    ""
+  ]
+  return (
+    <div className="bg-black pt-14">
+      <div className="z-0 absolute w-[300px] h-full">
+        <Image 
+          src="/Ellipse-6.png"
+          alt="tgf"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <h3 className="text-slate-200 font-mono ms-56 text-lg">OUR  SERVICES</h3>
+      <div className="flex">
+          <h1 className="text-[2.4rem] font-bold font-mono text-white leading-[4rem] ms-56">Revolutionize Your Industry with<br/>  AI and Robotics</h1>
+          <button className="w-52 h-14 bg-[#7B8CE5] rounded-full text-white ms-64 text-lg font-mono hover:bg-white hover:text-[#7B8CE5]">Discover More</button>  
+      </div>
+      <div className="flex justify-center gap-7 my-14 mr-13">
+          <Services_comp title="Automation"  description={data[0]} img_path="/auto.png"/>
+          <Services_comp title="Construction" description={data[0]} img_path="/pre.png"/>
+          <Services_comp title="Manufacturing" description={data[0]}img_path="/manu.png"/>
+      </div>
+      <div className="flex justify-center gap-7 mr-13">
+        <Services_comp title="Public Safety " description={data[0]} img_path="/safety.png"/>
+        <Services_comp title="Power Utilities" description={data[0]} img_path="/drill.png"/>
+        <Services_comp title="Management" description={data[0]} img_path="/pre.png"/>
+      </div> 
+    </div>
+  )
+}
+
+export default Page4
