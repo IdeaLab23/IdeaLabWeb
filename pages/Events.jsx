9345c85c6fa83c7b88a79d0ad925061fd0c95e45
lonewolf <bajpaiutkarsh1104@gmail.com>
2023-11-27 11:47:56 +0530

    made events responsive

--- pages/Events.jsx
+++ pages/Events.jsx
@@ -3,14 +3,14 @@ import EventsCardComp from './EventsCardComp'
 const Events = () => {
   return ( 
     <div className="bg-gray-900 text-white h-max ">    
-      <span className="text-lg p-5 ">Upcoming Events</span>
-      <div className='flex'>
+      <h1 className="text-4xl font-mono p-7">Upcoming Events</h1>
+      <div className='flex max-sm:flex-wrap'>
           <EventsCardComp/>
           <EventsCardComp/>
           <EventsCardComp/>
       </div>
-      <span className="text-lg p-5 ">Past Events</span>
-      <div className='flex'>
+      <h1 className="text-4xl font-mono p-7">Past Events</h1>
+      <div className='flex max-sm:flex-wrap'>
           <EventsCardComp/>
           <EventsCardComp/>
           <EventsCardComp/>
--- pages/EventsCardComp.jsx
+++ pages/EventsCardComp.jsx
@@ -2,21 +2,24 @@ import Image from "next/image"
 const EventsCardComp = () => {
   return (
     <>
-    <div className="flex p-5 ">
-     <span className='border-2 rounded-xl  w-36  h-20	border-color: rgb(161 161 170); p-3 text-center font-bold' >April 14</span>
-     <div className="ml-3">
-      <div className="flex   justify-between font-bold ">
-     <span >Q and A </span>
-         <span >7:00  to 9:00</span></div>
-        <span>Lorem ipsum dolor sit amet cons ecte tur adipi si cing elit. Labo riosam,  mini ma.</span>
-     <Image
-      src="/roboHand.jpg" width={500} alt="Image" height={10}
-     ></Image>
-     <div className="flex justify-between font-bold p-3 ">
-      <span>21 Seats left</span>
-     <button className='border-2 rounded  w-24 bg-indigo-950 '>Sign Up</button>
-     </div></div></div>
-     </>
+    <div className="flex p-5 max-sm:p-3 ">
+      <span className='border-2 rounded-xl w-36 h-20 border-color: rgb(161 161 170) flex flex-col justify-center text-center font-bold p-4 '>April 14</span>
+      <div className="ml-3">
+        <div className="flex justify-between font-bold text-lg ">
+          <span >Q and A</span>
+          <span >7:00  to 9:00</span>
+        </div>
+        <p className="text-xl pb-4">Lorem ipsum dolor sit amet cons ecte tur adipi si cing elit. Labo riosam,  mini ma.</p>
+        <Image
+          src="/roboHand.jpg" width={500} alt="Image" height={10}
+        />
+        <div className="flex justify-between font-bold p-3 text-lg ">
+          <span>21 Seats left</span>
+          <button className='border-2 rounded w-24 bg-indigo-950 p-1'>Sign Up</button>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
