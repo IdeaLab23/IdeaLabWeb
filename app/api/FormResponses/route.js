e91211e723dbc667c49deb810e9970a97d9b6d7c
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2024-03-05 08:56:32 +0530

    Add GET route for form responses

--- app/api/FormResponses/route.js
+++ app/api/FormResponses/route.js
@@ -2,6 +2,24 @@ import connectDB from "@/libs/mongodb"
 import FormResponse from "@/models/formResponsesSchema"
 import { NextResponse } from "next/server"
 
+//GET all form responses
+export async function GET() {
+  try {
+    await connectDB()
+    const formResponsesData = await FormResponse.find({})
+    return NextResponse.json(formResponsesData)
+  } catch (error) {
+    return NextResponse.json({
+      message: "Failed to get form responses",
+      error: error
+    },
+    {
+      status: 500
+    })
+  }
+}
+
+//POST new form response
 export async function POST(request) {
   try {
     const { name, email, phone, pitch, iot, dev, ui } = await request.json()
