eb4c79d9130464634fd26e8f5a9a434b5fc7d3a3
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2024-03-05 20:11:38 +0530

    admin page

--- pages/Events.jsx
+++ pages/Events.jsx
@@ -35,7 +35,7 @@ const Events = () => {
       <div className='flex justify-between items-center max-sm:flex-wrap max-sm:w-[35rem] '>
         <h1 className="text-4xl font-mono p-7">Upcoming Events</h1>
         <button className='border-2 rounded w-24 bg-[#111827] p-1 me-8 hover:bg-indigo-900'>
-          <Link href="/" >Home</Link>
+          <Link href="/">Home</Link>
         </button> 
       </div>
       <div className='flex flex-wrap'>
