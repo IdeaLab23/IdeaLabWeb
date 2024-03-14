6928cc6fdc7e1a20bba87551a3c94c8ae42c4a69
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2024-03-14 19:36:43 +0530

    o-auth removed

--- auth.js
+++
@@ -1,9 +0,0 @@
-import { NextAuthConfig } from "next-auth";
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-
-const config  = {
-   providers: [Google]
-} 
-
-export const {handlers, auth, signIn, signOut} = NextAuth(config);
