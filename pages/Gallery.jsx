8fd56fe535260a774615a41a64283bf55e0fd553
CoolSidOfficial <hackersiddhant1@gmail.com>
2024-03-28 19:26:17 +0530

    resp-9

--- pages/Gallery.jsx
+++ pages/Gallery.jsx
@@ -7,19 +7,19 @@ const Gallery = () => {
   return (
     <>
       {/* <Navbar/> */}
-      <video width="100%" height="100%"  autoPlay muted loop className="z-[-1] right-0 top-0 brightness-50">
+      <video width="100%" height="100%"  autoPlay muted loop className="z-[-1] xl:right-0 xl:top-0 brightness-50">
         <source src=" video/highway.mp4" type="video/mp4"></source>
       </video> 
-      <div className=" text-center text-white absolute top-48 bottom-16 right-40 left-40">
+      <div className=" text-center text-white absolute xl:top-48 xl:bottom-16 xl:right-40 xl:left-40 max-sm:top-10   ">
         <h1 className="font-bold text-6xl max-sm:">Welcome To IdeaLab</h1>
         <p className="text-2xl">Unlock your creative potential at IDEALab</p>
-        <a className="p-14 text-6xl hover:text-orange-500 "href="#first" > ↓</a>
+        <a className="p-14 text-6xl hover:text-orange-500 max-sm:hidden "href="#first" > ↓</a>
       </div >  
-      <div className="flex  bg-[#111827]"  id="first">
+      <div className="flex  bg-[#111827] max-sm:mt-5 max-sm:flex-col"  id="first">
         <Gallery_Card image="/idea_g/demon.jpeg" title="Demonstrations" />  
         <Gallery_Card image="/idea_g/workshop_f.jpeg " title="Workshops"/>
       </div> 
-      <div className="flex bg-[#111827]"> 
+      <div className="flex bg-[#111827] max-sm:mt-5 max-sm:flex-col"> 
         <Gallery_Card image="/idea_g/memories.jpeg" title="Memories"/>
         <Gallery_Card image="/idea_g/events_logo/3dtech.JPG" title="Posters"/>  
       </div>
--- pages/Gallery_Card.jsx
+++ pages/Gallery_Card.jsx
@@ -2,13 +2,13 @@ import Image from "next/legacy/image"
 
 const Gallery_Card = (props) => {
   return (
-    <a className="pointer text-white font-extrabold flex text-2xl m-5 flex-col hover:brightness-50 hover:scale-100" href="http://localhost:3000/gcards/Demonstration">
+    <a className="pointer text-white font-extrabold flex xl:text-2xl xl:m-5 flex-col hover:brightness-50 hover:scale-100 " href="http://localhost:3000/gcards/Demonstration">
     <Image className=" "
       src={props.image}
       width={600}
       height={500}
     />  
-    <div className="relative bottom-12 p-2 brightness-100 bg-black">
+    <div className="relative xl:bottom-12 xl:p-2 brightness-100 bg-black max-sm:bottom-84 max-sm:text-center">
       <span className="">{props.title}</span>
     <span>{props.desc}</span></div>
     </a>
