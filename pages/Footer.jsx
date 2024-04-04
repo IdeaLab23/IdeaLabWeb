38810983f11d377a1778067f5364b816c820c609
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2024-04-04 23:02:16 +0530

    footer

--- pages/Footer.jsx
+++ pages/Footer.jsx
@@ -4,17 +4,17 @@ const Footer = () => {
   return (
     <div>
         <footer className='bg-black  max-sm:text-center '>
-            <div className="flex flex-wrap justify-between pt-16 pb-8 xl:ps-24 max-sm:w-[24rem] max-sm:pt-18 ">
-                <div className="w-[20rem] max-sm:w-[30rem]  ">
-                    <h3 className="text-3xl text-white font-bold max-sm:text-[3.5rem] ">Address</h3>
-                    <div className="pt-6 text-lg font-medium text-[#959595] hover:text-slate-200 hover:cursor-pointer max-sm:text-2xl max-sm:leading-normal max-sm:py-10 max-sm:ms-10 ">
+            <div className="flex flex-wrap justify-between pt-16 pb-8 xl:ps-24 max-sm:w-full max-sm:pt-18 ">
+                <div className="w-[20rem] max-sm:w-full  ">
+                    <h3 className="text-2xl text-white font-bold max-sm:text-[2rem] ">Address</h3>
+                    <div className="pt-6 text-lg font-medium text-[#959595] hover:text-slate-200 hover:cursor-pointer max-sm:text-xl max-sm:leading-normal max-sm:py-4 ">
                        <Link href="https://maps.app.goo.gl/9G7zqUiAun3R1vbd7"> 📍AICTE IDEA Lab, LNCT <br /> Raisen Road, Kalchuri Nagar <br />Bhopal, Madhya Pradesh 462022</Link>
                     
                     </div>
                 </div>
-                <div className="w-[20rem] max-sm:w-[30rem] ">
-                    <h3 className="text-3xl text-white font-bold max-sm:leading-normal  max-sm:text-[3.5rem] ">Our Coordinators </h3>
-                    <ul className="pt-6 text-lg font-medium text-[#959595] max-sm:text-2xl max-sm:py-10 max-sm:leading-normal max-sm:ms-5 ">
+                <div className="w-[20rem] max-sm:w-full ">
+                    <h3 className="text-2xl text-white font-bold max-sm:leading-normal  max-sm:text-[2rem] ">Our Coordinators </h3>
+                    <ul className="pt-6 text-lg font-medium text-[#959595] max-sm:text-xl max-sm:py-4 max-sm:leading-normal max-sm:px-4 ">
                         <li className='hover:text-slate-200'><Link href="https://in.linkedin.com/in/dr-anand-singh-550488149"><b>Dr. Anand Singh</b>(Coordinator AICTE IDEA Lab, & HOD, Electrical & Electronic Department )</Link></li>
                        <br></br>
                         <li className='hover:text-slate-200'><Link href="https://in.linkedin.com/in/siju-george"><b>Prof.Siju George</b>(Coordinator AICTE IDEA Lab )</Link></li>
@@ -22,37 +22,37 @@ const Footer = () => {
                         <li className='hover:text-slate-200'><Link href="#contact"></Link></li>
                     </ul>
                 </div>
-                    <div className="w-[20rem] max-sm:w-[30rem] ">
-                    <h3 className="text-3xl text-white font-bold max-sm:text-[3.5rem]  ">Quick Links</h3>
-                    <ul className="pt-6 text-lg font-medium text-[#959595] max-sm:text-4xl max-sm:py-10 max-sm:leading-normal ">
+                    <div className="w-[20rem] max-sm:w-full ">
+                    <h3 className="text-2xl text-white font-bold max-sm:text-[2rem]  ">Quick Links</h3>
+                    <ul className="pt-6 text-lg font-medium text-[#959595] max-sm:text-xl max-sm:py-4 max-sm:leading-normal ">
                         <li className='hover:text-slate-200'><Link href="/">Home</Link></li>
                         <li className='hover:text-slate-200'><Link href="#aboutUs">About Us</Link></li>
                         <li className='hover:text-slate-200'><Link href="#">Projects</Link></li>
                         <li className='hover:text-slate-200'><Link href="#contact">Contact</Link></li>
                     </ul>
                 </div>
-                <div className="w-[20rem] max-sm:w-[30rem] ">
-                    <h3 className="text-3xl text-white font-bold max-sm:text-[3.5rem] ">Follow Us</h3>
-                    <ul className="pt-6 text-lg font-medium text-[#959595] hover:cursor-pointer max-sm:text-4xl max-sm:py-10 max-sm:leading-normal ">
+                <div className="w-[20rem] max-sm:w-full ">
+                    <h3 className="text-2xl text-white font-bold max-sm:text-[2rem] ">Follow Us</h3>
+                    <ul className="pt-6 text-lg font-medium text-[#959595] hover:cursor-pointer max-sm:text-xl max-sm:py-4 max-sm:leading-normal ">
                     <li className='hover:text-slate-200'><Link href="https://in.linkedin.com/company/aicte-idea-lab-lnct-bhopal">LinkedIn</Link></li>
                     <li className='hover:text-slate-200'><Link href="https://m.facebook.com/p/AICTE-IDEA-Lab-LNCT-Bhopal-100083871655159/">Facebook</Link></li>
                     {/* <li className='hover:text-slate-200'><Link href="https://twitter.com">Twitter</Link></li> */}
                     {/* <li className='hover:text-slate-200'><Link href="https://instagram.com">Instagram</Link></li> */}
                     </ul>
                 </div>
-                <div className="w-[20rem] max-sm:w-[30rem] ">
-                    <h3 className="text-3xl text-white font-bold max-sm:text-[3rem] ">Contact Us</h3>
-                    <ul className="pt-6 text-lg font-medium text-[#959595] hover:cursor-pointer max-sm:text-4xl max-sm:py-10 max-sm:leading-normal ">
+                <div className="w-[20rem] max-sm:w-full ">
+                    <h3 className="text-2xl text-white font-bold max-sm:text-[2rem] ">Contact Us</h3>
+                    <ul className="pt-6 text-lg font-medium text-[#959595] hover:cursor-pointer max-sm:text-xl max-sm:py-4 max-sm:leading-normal ">
                         <li className='hover:text-slate-200'>Phone</li>
                         <li className='hover:text-slate-200'><Link href="mailto:idealab@lnct.ac.in">Email</Link></li>
                     </ul>
                 </div>
           </div>
-          <div className="flex justify-between pt-8 pb-4 px-24 text-lg font-medium text-[#959595] max-sm:w-[100vw] max-sm:text-xl max-sm:px-12 max-sm:py-10 max-sm:ms-5 ">
+          <div className="flex justify-between pt-8 pb-4 px-24 text-lg font-medium text-[#959595] max-sm:w-full max-sm:text-xl max-sm:px-0 max-sm:py-10 ">
             <p>© 2023 All Rights Reserved.</p>
             <p>Terms & Conditions</p>
           </div>
-          <div className="bg-red-800 font-semi-bold text-white text-center py-4  bottom-0 w-full"> Website developed with  Love by <Link href="https://in.linkedin.com/in/siddhant-jain-coolsidofficial"> <b>Siddhant Jain</b> </Link>and <Link href="https://in.linkedin.com/in/utkarsh-bajpai-144925254"><b>Utkarsh Bajpai</b> </Link></div>
+          <div className="bg-red-800 font-semi-bold text-white text-center py-4  bottom-0 w-full"> Website developed by <Link href="https://in.linkedin.com/in/siddhant-jain-coolsidofficial"> <b className="underline">Siddhant Jain</b> </Link>and <Link href="https://in.linkedin.com/in/utkarsh-bajpai-144925254"><b className="underline">Utkarsh Bajpai</b> </Link></div>
         </footer>
     </div>
   )
