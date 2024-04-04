0f1e9febf5eb50cd4f5c4a647f04c8d62e487adc
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2024-04-04 18:11:00 +0530

    about tak theek.. video ka play button bacha

--- pages/AboutComp.jsx
+++ pages/AboutComp.jsx
@@ -11,7 +11,7 @@ const Page2Comp = (props) => {
           height="70px"
         />
         <h2 className="text-4xl font-bold my-7 max-sm:text-4xl max-sm:ms-18 max-sm:my-12 ">{props.title}</h2>
-        <p className="text-slate-200 text-lg text-justify max-sm:text-3xl max-sm:ms-18 max-sm:leading-normal max-sm:mb-16 max-sm:tracking-wide ">{props.text}</p>
+        <p className="text-slate-200 text-lg max-sm:text-3xl max-sm:ms-18 max-sm:leading-normal max-sm:mb-16 max-sm:tracking-wide ">{props.text}</p>
       </div>
     </div>
   )
--- pages/Navbar.jsx
+++ pages/Navbar.jsx
@@ -24,20 +24,20 @@ const Page1 = () => {
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
         </div>
-        <div className="block ms-[19rem] xl:hidden">
+        <div className="block ms-[22rem] xl:hidden">
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
+            className="flex items-center mt-12 rounded text-black-500 hover:text-black-400"
           >
             <svg
-              className={`fill-current h-16 w-16 ${isOpen ? "hidden" : "block"}`}
+              className={`fill-current h-10 w-10 ${isOpen ? "hidden" : "block"}`}
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
             </svg>
             <svg
-              className={`fill-current h-16 w-16 ${isOpen ? "block" : "hidden"}`}
+              className={`fill-current h-10 w-10 ${isOpen ? "block" : "hidden"}`}
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
@@ -72,7 +72,7 @@ const Page1 = () => {
           </p>
         </div>
       </div>          
-        <div className={`hidden absolute bg-[#151721] xl:w-[1000px] max-sm:text-white max-sm:font-mono max-sm:text-4xl max-sm:mt-36 max-sm:ps-10 max-sm:py-12 max-sm:${isOpen ? "block" : "hidden"}`}>
+        <div className={`hidden absolute bg-[#151721] max-sm:w-full max-sm:text-white max-sm:font-mono max-sm:text-2xl max-sm:mt-36 max-sm:ps-10 max-sm:py-12 max-sm:${isOpen ? "block" : "hidden"}`}>
           <p className="hover:text-[#7686DB] hover:cursor-pointer hover:underline-offset-auto max-sm:py-8">
             <Link href="/">Home</Link>
           </p>
